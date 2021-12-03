26dd3cb5b3ae0cb3a46b02f46a69526e6b0ddd6a
grace.tantra <gracextantra@gmail.com>
2021-12-02 20:26:51 -0800

    Update buttonPage.js

--- pages/buttonPage.js
+++ pages/buttonPage.js
@@ -3,23 +3,33 @@ import { StyleSheet, Text, ScrollView, StatusBar, TouchableOpacity } from 'react
 import { Button, Card } from 'react-native-elements';
 import call from 'react-native-phone-call';
 
-const ButtonPage = () => {
-  callNum = (num) => {
-    call(num).catch(console.error)
+  const ButtonPage = () => {
+    callNum = (num) => {
+        call(num).catch(console.error)
   }
 
   const SafewalkNum = {
-    number: "4085544410",
+    number:"" + "4085544410",
     prompt: false
   }
 
   const CampusSafetyNum = {
-    number: "4085544441",
+    number: "" + "4085544441",
     prompt: false
   }
 
   const CampusEmergencyNum = {
-    number: "4085544444",
+    number: "" + "4085544444",
+    prompt: false
+  }
+
+  const CAPSNum = {
+    number: "" + "4085544501",
+    prompt: false
+  }
+
+  const SupportLineNum = {
+    number: "" + "4085545220",
     prompt: false
   }
 
@@ -31,7 +41,8 @@ const ButtonPage = () => {
             <Card.Divider/>
             <Card.Image style={styles.image} resizeMode="cover" source={require('../assets/images/safewalk-logo.png')}/>
             <Text style={styles.description}>
-                On Wednesdays, Fridays, and Saturdays, SafeWalk has two people on shift who can help walk you home. This does not involve Campus Safety.
+                Safewalk is open Wednesday, Friday, and Saturday, from 10:00PM to 1:00AM. They have people on shift
+                who can help walk you home safely. Calls are kept confidential.
             </Text>
             <TouchableOpacity
                 style={styles.callButton}
@@ -43,7 +54,7 @@ const ButtonPage = () => {
             <Card.Title>SCU Campus Safety</Card.Title>
             <Card.Divider/>
             <Text style={styles.description}>
-                Campus Safety is open 24 hours a day. They work to keep the campus community safe, directly under SCU.
+                Campus Safety is open 24/7. They work to keep the campus community safe, directly under SCU.
             </Text>
             <TouchableOpacity
                 style={styles.callButton}
@@ -55,7 +66,34 @@ const ButtonPage = () => {
             <Card.Title>SCU Campus Safety Emergency</Card.Title>
             <Card.Divider/>
             <Text style={styles.description}>
-                These are also directed toward Campus Safety, which is open 24 hours a day. Please only call in case of an emergency.
+                Campus Safety's emergency phone line is open 24/7. Please only call in case of an emergency.
+            </Text>
+            <TouchableOpacity
+                style={styles.callButton}
+                onPress = {() => callNum(CampusEmergencyNum)}>
+                <Text style={styles.btnText}>Call number</Text>
+            </TouchableOpacity>
+        </Card>
+        <Card>
+            <Card.Title>SCU CAPS</Card.Title>
+            <Card.Divider/>
+            <Text style={styles.description}>
+                CAPS is open M-F, 8:30AM to 5:00PM. They provide students with mental health and wellness services.
+                After hours, their crisis line is accessible through Campus Safety's non-emergency number.
+            </Text>
+            <TouchableOpacity
+                style={styles.callButton}
+                onPress = {() => callNum(CampusEmergencyNum)}>
+                <Text style={styles.btnText}>Call number</Text>
+            </TouchableOpacity>
+        </Card>
+        <Card>
+            <Card.Title>24/7 Mental Health Support Line</Card.Title>
+            <Card.Divider/>
+            <Text style={styles.description}>
+                CAPS has partnered to have a 24/7 Support Line.
+                From 8:00AM to 5:00PM, the call will be answered by patient navigators.
+                From 5:00PM to 8:00AM, the call will be answered by licensed clinicians.
             </Text>
             <TouchableOpacity
                 style={styles.callButton}
