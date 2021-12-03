ab84e931265164d4e759ca7d44df048bb13915fc
grace.tantra <gracextantra@gmail.com>
2021-12-03 07:01:27 -0800

    Changed to expo-sms

--- pages/textPage.js
+++ pages/textPage.js
@@ -1,29 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Button, Card } from 'react-native-elements';
-import SendSMS from 'react-native-sms';
+//import SendSMS from 'react-native-sms';
+import * as SMS from 'expo-sms';
 
 const TextPage = () => {
     sendText = (num) => {
-        console.log('Text testing');
-        SendSMS.send({
-            body: '',
-            recipients: [num],
-            successTypes: ['sent', 'queued'],
-            allowAndroidSendWithoutReadPermission: true
-        }, (completed, cancelled, error) => {
-            console.log('SMS Callback: completed: ' + completed + ' cancelled: ' + cancelled + 'error: ' + error);
-        });
-        console.log('Text testing end');
+        const isAvailable = SMS.isAvailableAsync();
+        if (isAvailable) {
+          console.log('SMS is available');
+        } else {
+          console.log('SMS is unavailable');
+          return;
+        }
+
+        const { result } = SMS.sendSMSAsync(
+          [num]
+        );
     }
 
     const items = [
         <Card>
             <Card.Title>Emergency Contact Name</Card.Title>
-            <Button
-                title = "Send text"
-                onPress = {() => sendText('9876543210')}
-            />
+            <TouchableOpacity
+              style={styles.stackedButton}
+              onPress = {() => sendText('4089999996')}>
+              <Text style={styles.btnText}>Jessica Dinh</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.stackedButton}
+              onPress = {() => sendText('4089999997')}>
+              <Text style={styles.btnText}>Reanne Inafuku</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.stackedButton}
+              onPress = {() => sendText('4089999998')}>
+              <Text style={styles.btnText}>Josephine Lo</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.stackedButton}
+              onPress = {() => sendText('4089999999')}>
+              <Text style={styles.btnText}>Grace Tantra</Text>
+            </TouchableOpacity>
         </Card>
     ]
 
@@ -39,6 +57,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#e9e8d5',
     maxHeight:'100%',
     alignItems: 'center'
+  },
+  stackedButton: {
+    backgroundColor: '#b30738',
+    paddingTop: '4%',
+    paddingBottom: '4%',
+    marginTop: '3%',
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  btnText: {
+    color: '#ffff',
+    fontSize: 18
   },
 });
 
